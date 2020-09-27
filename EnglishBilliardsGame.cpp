417b88cdad47da5ef0dfcdc580491dbc44995548
Mike-R-R <Mike@DESKTOP-8GEUKMQ.localdomain>
2020-09-26 21:08:10 -0600

    Added EnglishBilliardsGame.cpp file.

---
+++ EnglishBilliardsGame.cpp
@@ -0,0 +1,56 @@
+
+#include "EnglishBilliardsGame.h"
+
+EnglighBilliardsGame::EnglighBilliardsGame(){
+	frameTimer.hours = 0;
+	framTimer.minutes = 0;
+	framTimer.seconds = 0;
+	timerRunning = true;
+	foul = false;
+	player1.set_at_table(true);
+}
+
+
+EnglighBilliardsGame::~EnglighBilliardsGame(){
+	
+}
+
+
+void EnglighBilliardsGame::get_player_scores(int *){
+	
+}
+
+
+void EnglighBilliardsGame::get_player_breaks(int *){
+	
+}
+
+
+void EnglighBilliardsGame::receive_point_intput(int){
+	
+}
+
+
+bool EnglighBilliardsGame::player_fouled(){
+	
+}
+
+
+void EnglighBilliardsGame::add_foul_points(int){
+	
+}
+
+
+void EnglighBilliardsGame::end_frame(){
+	
+}
+
+
+int EnglighBilliardsGame::shooting_player(){
+	
+}
+
+
+bool EnglighBilliardsGame::revert_game_state(){
+	
+}
